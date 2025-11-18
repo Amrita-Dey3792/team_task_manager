59fa215caac0ab94bd63ab70888d770f6a2d5e9c
Amrita-Dey3792 <amrita15-3792@diu.edu.bd>
2025-11-18 22:42:06 +0600

    add TaskViewSet with filtering, search, and ordering

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Task
+from .serializers import TaskSerializer
+
 
 # Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['status', 'assigned_to__user', 'due_date']
+    search_fields = ['title', 'description']
+    ordering_fields = ['created_at', 'due_date']
+
+    def get_queryset(self):
+        return Task.objects.filter(team__memberships__user=self.request.user, is_deleted=False)
+
