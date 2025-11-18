f8201d85b8ae4cdd687b8c2ac956d64fcc2ec64c
Amrita-Dey3792 <amrita15-3792@diu.edu.bd>
2025-11-18 23:08:58 +0600

    add RegisterSerializer and LoginSerializer for user authentication

---
+++ users/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, label="Confirm Password")
+
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'username', 'password', 'password2']
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match"})
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            name=validated_data.get('name', ''),
+            password=validated_data['password']
+        )
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        from django.contrib.auth import authenticate
+        user = authenticate(email=data['email'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid email or password")
+        if not user.is_active:
+            raise serializers.ValidationError("Account is disabled")
+        data['user'] = user
+        return data
